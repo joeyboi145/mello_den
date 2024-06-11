150679c27a4be2755cd122cf5394a27f3ad34fa2
Joe Martinez <joe.martinez@stonybrook.edu>
2024-06-11 09:43:30 -0400

    Profile page started

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,9 +1,31 @@
 import React from "react";
 
-export default function Profile(){
-    return(
+export default function Profile() {
+    return (
         <>
-            User Profile!
+            <section id='user_info'>
+                <div id='user' className="center">
+                    <div id='user_profile' className="center">
+                        Z
+                    </div>
+                    <p id='profile_username'
+                        className="peach_highlight center">
+                        <b>Username</b>
+                    </p>
+                </div>
+
+                <div id='user_stats'>
+                    <table>
+                        <tr>
+
+                        </tr>
+                    </table>
+                </div>
+            </section>
+
+            <hr></hr>
+
+            <div id='user_reviews'>No Reviews Left</div>
         </>
     )
 }
