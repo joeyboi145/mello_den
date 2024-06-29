240f6263f5a47baa0b806c4c36554f2698b4faf7
Joe Martinez <joe.martinez@stonybrook.edu>
2024-06-29 11:30:30 -0400

    CLIENT: Added Loader.js ServerDown.js elements for loading information display

---
+++ client/src/components/Loader.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+export default function Loader(props) {
+    return (
+        <div className="loader" style={{ backgroundColor: props.color }} >
+        </div>
+    )
+}
---
+++ client/src/pages/ServerDown.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default function ServerDown() {
+    return (
+        <>
+            <br></br>
+            <br></br>
+            <br></br>
+            <h2 className="highlight_title">Server Down for Maintenance</h2>
+
+            <p style={{fontSize: '20px'}}>
+                We are working on fixing this as soon as possible. Thank
+                you for your understanding.
+            </p>
+
+        </>
+    )
+}
