8f2d39475393bb7ed582023b9abad15adbe2e554
Joe Martinez <joe.martinez@stonybrook.edu>
2024-06-29 11:31:59 -0400

    SERVER: updated Stat schema to include validation

--- server/models/Stat.js
+++ server/models/Stat.js
@@ -1,15 +1,53 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+const MAX_HYDRATION_LEVEL = 10;
+const MAX_SLEEP = 24;
+const MAX_SUNSCREEN = 12;
+
 const statSchema = mongoose.Schema({
-    hydration_level: {type: Number, required: true},
-    meals: {type: Number, required: true},
-    sleep: {type: Number, required: true},
-    suncreen: {type: Number, required: true},
-    
-    completed_by: {type: Schema.Types.ObjectID, ref: "userSchema", required: true},
-    completion_time: {type: Date, default: new Date()},
-    completed: {type: Boolean, default: false}
+    hydration_level: {
+        type: Number,
+        required: [true, "Please enter a hydration level."],
+        validate: [val => (val >= 0 && val <= MAX_HYDRATION_LEVEL),
+        `Hydration level can only be from a range of 0-${MAX_HYDRATION_LEVEL}`]
+    },
+    meals: {
+        meal_1: {
+            type: Boolean,
+            required: [true, "Please indicate if you ate at your first meal of the day."]
+        },
+        meal_2: {
+            type: Boolean,
+            required: [true, "Please indicate if you are your second meal of the day."]
+        },
+        breakfast: {
+            type: Boolean,
+            required: [true,
+                "Please indicate if your first meal is what you would consider your breakfast."]
+        },
+    },
+
+    sleep: {
+        type: Number,
+        required: [true, "Please enter how many hours you slept last night."],
+        validate: [val => (val >= 0 && val <= MAX_SLEEP),
+        `Hours of sleep can only range from 0-${MAX_SLEEP}`]
+    },
+
+    suncreen: {
+        type: Number,
+        required: [true, "Please enter the amount of times you have put sunscreen on today."],
+        validate: [val => (val >= 0 && val <= MAX_SUNSCREEN),
+        `Sunscreen application amount can only be in a range from 0-${MAX_SUNSCREEN}`]
+    },
+
+    done_by: {
+        type: Schema.Types.ObjectID, ref: "userSchema",
+        required: [true, "Please provide user credentials."]
+    },
+    done_at: { type: Date, default: new Date() },
+    completed: { type: Boolean, default: false }
 });
 
 module.exports = mongoose.model('Stat', statSchema);
