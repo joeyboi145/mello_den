1fe6a67a76bb6ff1cb16c6821c04304d59180bdb
Joe Martinez <joe.martinez@stonybrook.edu>
2024-06-30 00:17:35 -0400

    CLIENT: added notification bar

---
+++ client/src/components/NotificationBar.js
@@ -0,0 +1,34 @@
+import React, { useContext, useEffect, useReducer, useRef, useState } from "react";
+import { NotificationContext } from "../App";
+
+export default function NotificationBar() {
+    const { notification, setNotification } = useContext(NotificationContext)
+    const [animationClass, setAnimationClass] = useState('notification-in');
+
+    const color = notification.error ? '#f4d8b9' : '#D7CFCD';
+
+    useEffect(() => {
+        const animate = setTimeout(() => {
+            setAnimationClass("notification-out")
+        }, 4000);
+        const remove = setTimeout(() => {
+            setNotification({
+                display: false,
+                message: '',
+                error: false
+            });
+        }, 5000)
+        return () => {
+            clearTimeout(animate)
+            clearTimeout(remove)
+        }
+    }, [notification.display]);
+
+    return (
+        <div className={`notification ${animationClass}`}
+            style={{ backgroundColor: color }}>
+            <p>{notification.message}</p>
+        </div>
+    );
+
+}
