7dc99c3448fc522ef3ef8d8b08f4fce3811d8e05
Joe Martinez <joe.martinez@stonybrook.edu>
2024-07-05 18:41:18 -0400

    CLIENT: fixed deadline bug where it won't reset, added waves svg to top

--- src/components/StatTimer.js
+++ src/components/StatTimer.js
@@ -7,44 +7,49 @@ const MINUTE = SECOND * 60;
 const HOUR = MINUTE * 60;
 const DAY = HOUR * 24;
 
-let date = new Date();
-let deadline = new Date(date - (date % DAY) + DAY);
+const defineDeadline = () => {
+    let date = new Date();
+    return new Date(date - (date % DAY) + DAY);
+}
 
 export default function StatTimer() {
+    const [deadline, setDeadline] = useState(defineDeadline())
     const [timespan, setTimespan] = useState(new Date(deadline) - Date.now());
-    
+
     useEffect(() => {
         const intervalId = setInterval(() => {
-            if (new Date(deadline) - Date.now() < 0){
-                deadline = new Date(date - (date % DAY) + DAY);;
-            }
+            if (deadline - Date.now() < 0) {
+                let newDeadline = defineDeadline();
+                setDeadline(newDeadline);
+                setTimespan(new Date(newDeadline - Date.now()));
+            } else setTimespan(new Date(deadline - Date.now()));
+        }, 1_000);
 
-            setTimespan(new Date(deadline) - Date.now());
-          }, 1_000);
-      
-    
         return () => {
-          clearInterval(intervalId);
+            clearInterval(intervalId);
         };
-      }, []);
+    });
 
     return (
-        <div id='stat_timer_section'>
-            <p> The Next Winner will be chosen in:</p>
-            <div id='stat_timer'>
-                <div id='stat_timer_hours'>
-                    {Math.floor(timespan / HOUR)}
+        <>
+            <div className='spacer winner_box_top'></div>
+            <div id='stat_timer_section'>
+                <p> The Next Winner will be chosen in:</p>
+                <div id='stat_timer'>
+                    <div id='stat_timer_hours'>
+                        {Math.max(0, Math.floor(timespan / HOUR))}
+                    </div>
+
+                    <div id='stat_timer_minutes'>
+                        {Math.max(0, Math.floor((timespan / MINUTE) % 60))}
+                    </div>
+
+                    <div id='stat_timer_seconds'>
+                        {Math.max(0, Math.floor((timespan / SECOND) % 60))}
+                    </div>
                 </div>
 
-                <div id='stat_timer_minutes'>
-                    {Math.floor((timespan / MINUTE) % 60)}
-                </div>
-
-                <div id='stat_timer_seconds'>
-                    {Math.floor((timespan / SECOND) % 60)}
-                </div>
             </div>
-
-        </div>
+        </>
     )
 }
