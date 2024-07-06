288d64cc44ca14a9a8ca0443332ed8544dccda3a
Joe Martinez <joe.martinez@stonybrook.edu>
2024-07-05 21:54:18 -0400

    CLIENT: added statform final score box

--- src/components/StatCheckForm.js
+++ src/components/StatCheckForm.js
@@ -36,7 +36,7 @@ export default function StatCheckForm() {
                     let statForm = res.data
                     var notification = createNotification("Retrieved previous form")
                     setForm(statForm);
-                    setNotification(notification);
+                    if (!statForm.completed) setNotification(notification);
                     setLoading(false);
                 })
                 .catch(err => {
@@ -95,6 +95,7 @@ export default function StatCheckForm() {
         });
     }
 
+
     if (!currentUser.login) {
         return (<div className='stats-credentials-box'>
             <p>Log in to complete your form!</p>
@@ -111,94 +112,97 @@ export default function StatCheckForm() {
         </div>)
     } else if (form.completed) {
         return (
-            <div className='total-score-box'>{getTotalScore(form)}</div>
+            <>
+                <p>Your score today:</p>
+                <div className='total-score-box animate-main-color-change'>{getTotalScore(form)}</div>
+            </>
         )
-    }
-
-    return (
-        // FIXME: move id SUBMIT FORM down a level
-        <div id='submit_form'>
-            <form>
-                {/* Hydration */}
-                <div id='hydration'>
-                    <div id='hydration_question'>
-                        <p> What is your hydration chart today?</p>
-                        <fieldset id='hydration_chart'>
-                            <HydrationEntry value={4} color='#fff8b9' form={{ form, handleFormChange }} />
-                            <HydrationEntry value={3} color='#fff177' form={{ form, handleFormChange }} />
-                            <HydrationEntry value={2} color='#f5e762' form={{ form, handleFormChange }} />
-                            <HydrationEntry value={1} color='#f6cd51' form={{ form, handleFormChange }} />
-                            <HydrationEntry value={0} color='#cb9800' form={{ form, handleFormChange }} />
-                        </fieldset>
-                    </div>
-                    <div id='hydration_score' className='stat_score_box'>
-                        <b>{form.hydration_level * 2}</b>
-                    </div>
-                </div>
-
-                {/* Meals Taken */}
-                <div id='meals'>
-                    <div id='meals_question'>
-                        <p> How many meals did you have today? </p>
-                        <div id='meals_row'>
-                            <MealEntry mealName="meal_1" form={{ form, handleMealChange }} />
-                            <MealEntry mealName="meal_2" form={{ form, handleMealChange }} />
-                            <MealEntry mealName="breakfast" form={{ form, handleMealChange }} />
+    } else if (currentUser.login && currentUser.verified && !loading) {
+        return (
+            // FIXME: move id SUBMIT FORM down a level
+            <div id='submit_form'>
+                <form>
+                    {/* Hydration */}
+                    <div id='hydration'>
+                        <div id='hydration_question'>
+                            <p> What is your hydration chart today?</p>
+                            <fieldset id='hydration_chart'>
+                                <HydrationEntry value={4} color='#fff8b9' form={{ form, handleFormChange }} />
+                                <HydrationEntry value={3} color='#fff177' form={{ form, handleFormChange }} />
+                                <HydrationEntry value={2} color='#f5e762' form={{ form, handleFormChange }} />
+                                <HydrationEntry value={1} color='#f6cd51' form={{ form, handleFormChange }} />
+                                <HydrationEntry value={0} color='#cb9800' form={{ form, handleFormChange }} />
+                            </fieldset>
+                        </div>
+                        <div id='hydration_score' className='stat_score_box'>
+                            <b>{form.hydration_level * 2}</b>
                         </div>
                     </div>
-                    <div id='meals_score' className='stat_score_box'>
-                        <b>{getMealScore(form)}</b>
-                    </div>
-                </div>
 
-                {/* Hours of sleep */}
-                <div id='sleep'>
-                    <div id='sleep_question'>
-                        <label htmlFor='sleep_entry'>Hours of sleep: </label>
-                        <input type="range"
-                            min="0"
-                            max="10"
-                            value={form.sleep}
-                            id="sleep_entry"
-                            name='sleep'
-                            onChange={handleFormChange} />
-                    </div>
-                    <div id='sleep_score' className='stat_score_box'>
-                        <b>{form.sleep * 3}</b>
-                    </div>
-                </div>
-
-                {/* Sunscreen */}
-                <div id='sunscreen'>
-                    <div id='sunscreen_question'>
-                        <label htmlFor='sunscreen_entry'>How many times did you put sunscreen: </label>
-                        <input type="range"
-                            min="0"
-                            max="8"
-                            value={form.sunscreen}
-                            id="sunscreen_entry"
-                            name='sunscreen'
-                            onChange={handleFormChange} />
-                    </div>
-                    <div id='sunscreen_score' className='stat_score_box'>
-                        <b>{form.sunscreen * 2}</b>
-                    </div>
-                </div>
-
-                {/* TOTAL Scoring */}
-                <div id='scoring'>
-                    <input type='submit'
-                        id='stats_submit_button'
-                        onClick={handleClick}>
-                    </input>
-                    <div id='scoring_points'>
-                        <h4 id='score_title'> TOTAL POINTS: </h4>
-                        <div id='final_score' className='stat_score_box'>
-                            <b>{getTotalScore(form)}</b>
+                    {/* Meals Taken */}
+                    <div id='meals'>
+                        <div id='meals_question'>
+                            <p> How many meals did you have today? </p>
+                            <div id='meals_row'>
+                                <MealEntry mealName="meal_1" form={{ form, handleMealChange }} />
+                                <MealEntry mealName="meal_2" form={{ form, handleMealChange }} />
+                                <MealEntry mealName="breakfast" form={{ form, handleMealChange }} />
+                            </div>
+                        </div>
+                        <div id='meals_score' className='stat_score_box'>
+                            <b>{getMealScore(form)}</b>
                         </div>
                     </div>
-                </div>
-            </form>
-        </div>
-    )
+
+                    {/* Hours of sleep */}
+                    <div id='sleep'>
+                        <div id='sleep_question'>
+                            <label htmlFor='sleep_entry'>Hours of sleep: </label>
+                            <input type="range"
+                                min="0"
+                                max="10"
+                                value={form.sleep}
+                                id="sleep_entry"
+                                name='sleep'
+                                onChange={handleFormChange} />
+                        </div>
+                        <div id='sleep_score' className='stat_score_box'>
+                            <b>{form.sleep * 3}</b>
+                        </div>
+                    </div>
+
+                    {/* Sunscreen */}
+                    <div id='sunscreen'>
+                        <div id='sunscreen_question'>
+                            <label htmlFor='sunscreen_entry'>How many times did you put sunscreen: </label>
+                            <input type="range"
+                                min="0"
+                                max="8"
+                                value={form.sunscreen}
+                                id="sunscreen_entry"
+                                name='sunscreen'
+                                onChange={handleFormChange} />
+                        </div>
+                        <div id='sunscreen_score' className='stat_score_box'>
+                            <b>{form.sunscreen * 2}</b>
+                        </div>
+                    </div>
+
+                    {/* TOTAL Scoring */}
+                    <div id='scoring'>
+                        <input type='submit'
+                            id='stats_submit_button'
+                            onClick={handleClick}>
+                        </input>
+                        <div id='scoring_points'>
+                            <h4 id='score_title'> TOTAL POINTS: </h4>
+                            <div id='final_score' className='stat_score_box'>
+                                <b>{getTotalScore(form)}</b>
+                            </div>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        )
+    } else return
 }
