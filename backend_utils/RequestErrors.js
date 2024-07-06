ba4bf8755ac0bfe7374b8d67d1c017951ab86c08
Joe Martinez <joe.martinez@stonybrook.edu>
2024-07-06 09:39:54 -0400

    moved backend utilities to backend_utils

---
+++ backend_utils/RequestErrors.js
@@ -0,0 +1,144 @@
+
+module.exports = {
+    /**
+     * Sends a general 500 Internal Server Error response to the client
+     * @param {Response} res Express Response object
+     * @param {Error} err Error object raised
+     */
+    handleServerError: (res, err) => {
+        var errors = { server: 'Server Error. Try again later' };
+        server_status = 'WARNING';
+        console.log(`FAILED: Server error...\n`, err);
+        res.status(500).json({ errors });
+    },
+
+    /**
+     * Sends a 400 Bad Request response to the client indicating
+     * that a request was sent without any credentials. 
+     * @param {Response} res Express Response object
+     */
+    handleCredentialsError:  (res) => {
+        var errors = { credentials: 'No credentials found. Please log in.' }
+        res.status(400).json({ errors })
+        console.log(`FAILED: No credentials sent\n`)
+    },
+
+    /**
+     * Sends a 401 Unauthorized response to the client indicating
+     * that a request was sent without proper authorization. 
+     * @param {Response} res Express Response object
+     */
+    handleAuthorizationError: (res) => {
+        var errors = { authorization: 'Missing proper authorization to perform request. Same user or admin privilages required' }
+        res.status(401).json({ errors })
+        console.log(`FAILED: Unauthorized request \n`)
+    },
+
+    /**
+     * Sends a 500 Internal Server Error response to client indicating 
+     * that a user was not found in the database.
+     * @param {Response} res Express Response object
+     */
+    handleUserQueryError: (res) => {
+        var errors = { user: "User not found. Try registering" }
+        res.status(500).json({ errors });
+        console.log(`FAILED: User not found\n`)
+    },
+
+    /**
+     * Takes a user registration error and sends out a 400 Bad Request response
+     * to the client depending on the type of error raised.
+     * @param {Response} res Express Response object
+     * @param {Error} err Error object raised by user registration
+     */
+    handleRegistrationErrors: (res, err) => {
+        console.log(err.message, err.code)
+        let errors = { email: '', username: '', password: '' }
+
+        // Dupilcate Error
+        if (err.code === 11000 && err.message.includes('index: email_1')) {
+            errors.email = 'Email already registered'
+        } else if (err.code === 11000 && err.message.includes('index: username_1')) {
+            errors.username = 'Username already used'
+        }
+
+        // Validation errors
+        if (err.message.includes('User validation failed')) {
+            Object.values(err.errors).forEach(({ properties }) => {
+                errors[properties.path] = properties.message;
+            })
+        }
+        res.status(400).json({ errors });
+        console.log(`FAILED: New user NOT created\n`)
+    },
+
+    /**
+     * Sends a 400 Bad Request response to client indicating that the
+     * requesting user is already verified and can't request a resources. 
+     * @param {Response} res Express Response object
+     */
+    handleVerificationError: (res) => {
+        var errors = { verified: 'User already verified' }
+        res.status(400).json({ errors })
+        console.log('FAILED: User already verified\n')
+    },
+
+    /**
+     * Sends a 400 Bad Request response to client indicating that the
+     * requesting user is NOT verified and can't request a resources. 
+     * @param {Response} res Express Response object
+     */
+    handleUnverifiedError: (res) => {
+        var errors = { unverified: 'User is not verified' }
+        res.status(400).json({ errors })
+        console.log('FAILED: User is not verified\n')
+    },
+
+    /**
+     * Sends a 400 Bad Request response to client indicating formatting/validation
+     * errors, depending on the type of error raised
+     * @param {Response} res Express Response object
+     * @param {Error} err Error object raised by Stat creation/updating
+     */
+    handleStatFormError: (res, err) => {
+        console.log(err.message, err.code)
+        let errors = {
+            done_by: '',
+            hydration_level: '',
+            meal_1: '',
+            meal_2: '',
+            breakfast: '',
+            sleep: '',
+            suncreen: ''
+        }
+
+        // Validation errors
+        if (err.message.includes('Stat validation failed')) {
+            Object.values(err.errors).forEach(({ properties }) => {
+                errors[properties.path] = properties.message;
+            })
+        }
+        res.status(400).json({ errors });
+        console.log(`FAILED: Unable to updating/creating Stat form\n`)
+    },
+
+
+    handleEmailLimitError: (res) => {
+        var errors = { email: "Unable to send email. User has reached email request limit. Try again in a couple of hours."}
+        res.status(400).json({ errors })
+        console.log('FAILED: User unable to send email due to limit\n')
+    },
+
+    handleVerifyLimitError: (res) => {
+        var errors = { verification: "Unable to verify user. User has reached verification limit for this code. Try requesing a new verification code" }
+        res.status(400).json({ errors })
+        console.log('FAILED: User unable to verify due to limit\n')
+    },
+
+    handleExpiredVerificationError: (res) => {
+        var errors = { verification: "Unable to verify user. Verification code has expired. Request a new verification code through email" }
+        res.status(400).json({ errors })
+        console.log('FAILED: User unable to verify due to limit\n')
+    }
+
+}
---
+++ backend_utils/functions.js
@@ -0,0 +1,121 @@
+
+const User = require('./src/models/User.js');
+const StatForm = require('./src/models/StatForm.js');
+const Announcement = require('./src/models/Announcement.js');
+const Event = require('./src/models/Event.js');
+const EmailRecord = require('./src/models/EmailRecord.js')
+const VerificationToken = require('./src/models/VerificationToken.js');
+
+
+module.exports = {
+    calculateDeadline: () => {
+        let date = new Date();
+        return new Date(date - (date % DAY) + DAY);
+    },
+
+    createSession: (req, user) => {
+        req.session.login = true;
+        req.session.userID = user._id;
+        req.session.username = user.username;
+        req.session.verified = user.verfied;
+        req.session.admin = user.admin;
+    },
+
+    createUserInfo: (login = false, username = '', verified = false, admin = false) => {
+        return {
+            user: {
+                login: login,
+                username: username,
+                verified: verified,
+                admin: admin
+            }
+        }
+    },
+
+    ableToSendEmail: async (user, res) => {
+        const record = await EmailRecord.findOne({ user: user._id });
+        if (record) {
+            if (record.count <= 0) {
+                Request.handleEmailLimitError(res);
+                return false
+            } else {
+                await EmailRecord.findByIdAndUpdate(record._id, { count: record.count - 1 });
+                return record
+            }
+        } else {
+            const newRecord = await EmailRecord.create({ user: user._id });
+            return newRecord;
+        }
+    },
+
+    ableToVerify: async (user, res) => {
+        const tokenRecord = await VerificationToken.findOne({ user: user._id });
+        if (tokenRecord) {
+            if (tokenRecord.tries <= 0) {
+                RequestErrors.handleVerifyLimitError(res);
+                return false;
+            } else {
+                await VerificationToken.findByIdAndUpdate(tokenRecord._id, { tries: tokenRecord.tries - 1 });
+                return tokenRecord;
+            }
+        } else {
+            RequestErrors.handleExpiredVerificationError(res);
+            return false;
+        }
+    },
+
+    getScore: (statForm) => {
+        let score = (statForm.hydration_level * 2)
+            + (statForm.sleep * 3)
+            + (statForm.sunscreen * 2)
+        if (statForm.meals.meal_1) score += 20;
+        if (statForm.meals.meal_2) score += 20;
+        if (statForm.meals.breakfast) score += 1;
+        return score
+    },
+
+    findUser: async (username)  => {
+        let user = await User.findOne({ username });
+        if (!user) throw new Error("User not found");
+        else return user
+    },
+    
+    createNewUser: async (username, email, pass = 'password') => {
+        let user = await User.create({
+            username: username,
+            email: email,
+            password: pass
+        });
+        return user
+    },
+
+    createNewStatForm: async (userID, data) => {
+        return await StatForm.create({
+            hydration_level: parseInt(Math.random() * 3),
+            meals: {
+                meal_1: false,
+                meal_2: false,
+                breakfast: false
+            },
+            sleep: parseInt(Math.random() * 8),
+            sunscreen: parseInt(Math.random() * 10),
+            done_by: userID,
+            done_at: date
+        })
+    },
+
+    determineWinner: async (winner_form, username, score) => {
+        if (score > winner_form.score) {
+            return {
+                username: username,
+                score: score
+            }
+        } else if (score === winner_form.score) {
+            return {
+                username: winner_form.username + ", " + username,
+                score: score
+            }
+        } else return winner_form
+    }
+
+}
